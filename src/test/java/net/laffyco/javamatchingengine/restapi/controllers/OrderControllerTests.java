7abe094c60c2753ff7fce99883db505104d71c5f
unknown <tom@laffyco.net>
2021-11-21 14:00:02 +0000

    Update repo

--- src/test/java/net/laffyco/javamatchingengine/restapi/controllers/OrderControllerTests.java
+++ src/test/java/net/laffyco/javamatchingengine/restapi/controllers/OrderControllerTests.java
@@ -1,28 +1,28 @@
-package net.laffyco.javamatchingengine.restapi.controllers;
-
-import javax.annotation.Resource;
-
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-
-import net.laffyco.javamatchingengine.core.api.OrderInterface;
-
-/**
- * Tests the OrderController.
- *
- * @author Laffini
- *
- */
-public class OrderControllerTests {
-
-    @Mock
-    private OrderInterface orderInterface;
-
-    @InjectMocks
-    @Resource
-    private OrderController orderController;
-
-    // TODO: Tests for each end point verifying that the correct method in the
-    // OrderInterface was called.
-
-}
+//package net.laffyco.javamatchingengine.restapi.controllers;
+//
+//import javax.annotation.Resource;
+//
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//
+//import net.laffyco.javamatchingengine.core.api.OrderInterface;
+//
+///**
+// * Tests the OrderController.
+// *
+// * @author Laffini
+// *
+// */
+//public class OrderControllerTests {
+//
+//    @Mock
+//    private OrderInterface orderInterface;
+//
+//    @InjectMocks
+//    @Resource
+//    private OrderController orderController;
+//
+//    // TODO: Tests for each end point verifying that the correct method in the
+//    // OrderInterface was called.
+//
+//}
--- src/test/java/net/laffyco/javamatchingengine/restapi/controllers/SpreadControllerTests.java
+++ src/test/java/net/laffyco/javamatchingengine/restapi/controllers/SpreadControllerTests.java
@@ -1,28 +1,28 @@
-package net.laffyco.javamatchingengine.restapi.controllers;
-
-import javax.annotation.Resource;
-
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-
-import net.laffyco.javamatchingengine.core.api.SpreadInterface;
-
-/**
- * Tests the SpreadController.
- *
- * @author Laffini
- *
- */
-public class SpreadControllerTests {
-
-    @Mock
-    private SpreadInterface spreadInterface;
-
-    @InjectMocks
-    @Resource
-    private SpreadController spreadController;
-
-    // TODO: Tests for each end point verifying that the correct method in the
-    // SpreadController was called.
-
-}
+//package net.laffyco.javamatchingengine.restapi.controllers;
+//
+//import javax.annotation.Resource;
+//
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//
+//import net.laffyco.javamatchingengine.core.api.SpreadInterface;
+//
+///**
+// * Tests the SpreadController.
+// *
+// * @author Laffini
+// *
+// */
+//public class SpreadControllerTests {
+//
+//    @Mock
+//    private SpreadInterface spreadInterface;
+//
+//    @InjectMocks
+//    @Resource
+//    private SpreadController spreadController;
+//
+//    // TODO: Tests for each end point verifying that the correct method in the
+//    // SpreadController was called.
+//
+//}
